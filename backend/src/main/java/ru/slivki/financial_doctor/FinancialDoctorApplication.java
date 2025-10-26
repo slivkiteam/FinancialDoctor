7725029d7d4ab078f528f19b0c993a4f9be8bc88
iluvatarrr <kds02122004@mail.ru>
2025-10-26 15:18:46 +0500

    main_feat: init proj with dc and actions

---
+++ backend/src/main/java/ru/slivki/financial_doctor/FinancialDoctorApplication.java
@@ -0,0 +1,13 @@
+package ru.slivki.financial_doctor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialDoctorApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(FinancialDoctorApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/ru/slivki/financial_doctor/FinancialDoctorApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.slivki.financial_doctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialDoctorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
